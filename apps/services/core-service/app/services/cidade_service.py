076679c3e3f022c675b3de4942d67b589c722205
Micael <micakaellgomess@gmail.com>
2026-09-22 08:24:00 -0300

    feat: implementa service de Cidade (#14)

---
+++ apps/services/core-service/app/services/cidade_service.py
@@ -0,0 +1,13 @@
+from app.services.hotel_service import (
+    CidadeJaExisteError,
+    CidadeNaoEncontradaError,
+    CidadeService,
+    RegraDeNegocioError,
+)
+
+__all__ = [
+    "CidadeJaExisteError",
+    "CidadeNaoEncontradaError",
+    "CidadeService",
+    "RegraDeNegocioError",
+]
